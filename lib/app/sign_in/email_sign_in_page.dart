298230ecb1fc0fce375416395d7b3fe47e782320
shakti051 <shaktithemastermind@gmail.com>
2021-06-04 20:03:06 +0530

    demo

--- lib/app/sign_in/email_sign_in_page.dart
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker/app/sign_in/email_sign_in_form_change_notifier.dart';
 
 class EmailSignInPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
